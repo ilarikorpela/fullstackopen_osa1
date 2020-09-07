152be448a1e696ab892c575f6e1acf89af871e6f
Ilari <ilari.tkorpela@gmail.com>
2020-09-07 13:30:47 +0300

    1.6 final

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,44 +1,31 @@
 import ReactDOM from "react-dom";
 import React, { useState } from "react";
 
-const Display = (props) => <div>{props.value}</div>;
-
-const Button = (props) => (
-  <button onClick={props.handleClick}>{props.text}</button>
-);
-
 const Header = (props) => {
   return <h1>{props.header}</h1>;
 };
+
+const Button = (props) => {
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
+
 const App = () => {
-  // tallenna napit omaan tilaansa
   const header = "Give feedback";
   const statistics = "Statistics";
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const handleGoodGrade = () => {
-    const newGoodGrade = good + 1;
-    setGood(newGoodGrade);
-  };
-
-  const handleNeutralGrade = () => {
-    const newNeutralGrade = neutral + 1;
-    setNeutral(newNeutralGrade);
-  };
-
-  const handleBadGrade = () => {
-    const newBadGrade = bad + 1;
-    setBad(newBadGrade);
-  };
+  const goodGrade = () => setGood(good + 1);
+  const neutralGrade = () => setNeutral(neutral + 1);
+  const badGrade = () => setBad(bad + 1);
 
   return (
     <div>
       <Header header={header} />
-      <button onClick={handleGoodGrade}>good</button>
-      <button onClick={handleNeutralGrade}>neutral</button>
-      <button onClick={handleBadGrade}>bad</button>
+      <Button handleClick={goodGrade} text="Good" />
+      <Button handleClick={neutralGrade} text="Neutral" />
+      <Button handleClick={badGrade} text="Bad" />
       <Header header={statistics} />
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
